2bd5798ec1ee0ba4309a5dad4d6e72a374264810
Ethan Perez <ethan@ethanperez.com>
2014-01-22 22:44:07 -0600

    Altered rides admin display for better categorization

--- fitness/admin.py
+++ fitness/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from fitness.models import Ride
 
+# Settings for ride admin
+class RideAdmin(admin.ModelAdmin):
+
+    # Fields to list
+    list_display = ('user', 'date', 'duration', 'miles', 'pace')
+    # Fields to allow filtering by
+    list_filter = ('date',)
+    # Fields to seach in with search bay
+    search_fields = ('user__first_name', 'user__last_name')
+    # Fields to order by
+    ordering = ('date', 'miles', 'duration', 'pace')
+
 # Add rides to admin panel
-admin.site.register(Ride)
+admin.site.register(Ride, RideAdmin)
