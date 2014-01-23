e9698bf5fb8d7b3ecce75853271bbc9b2aea736a
Ethan Perez <ethan@ethanperez.com>
2014-01-22 21:39:58 -0600

    Created 'teammate' model and admin

--- riders/admin.py
+++ riders/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from riders.models import Teammate, Title, Route
+from riders.forms import TeammateCreationForm, TeammateChangeForm
 
-# Register your models here.
+class TeammateAdmin(UserAdmin):
+    
+    # The forms to add and change user instances
+    form = TeammateChangeForm
+    add_form = TeammateCreationForm
+    
+    # The attributes of each teammate that are displayed
+    list_display = ('email', 'first_name', 'last_name', 'title', 'route', 'is_staff')
+    list_filter = ('is_staff', 'route')
+    
+    # Fields that are shown in edit user pane
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Information', {'fields': ('first_name', 'last_name', 'date_of_birth')}),
+        ('Team Information', {'fields': ('title', 'route')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',
+                          'user_permissions')}),
+    )
+    
+    # Fields that are shown in create user pane
+    add_fieldsets = (
+        (None, {'classes': ('wide,'),
+        'fields': ('email', 'first_name', 'last_name', 'title', 'route',
+        'date_of_birth', 'password1', 'password2')}
+        ),
+    )
+    
+    # Fields that are searched through search bar
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('first_name', 'last_name')
+    filter_horizontal = ('groups', 'user_permissions')
+    
+# Take the above mention changes into effect
+admin.site.register(Teammate, TeammateAdmin)
+admin.site.register(Title)
+admin.site.register(Route)
+    
--- riders/models.py
+++ riders/models.py
@@ -1,3 +1,113 @@
 from django.db import models
+from django.utils import timezone
+# For teammate manager and modification of Django's base user auth system
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+# Model that holds the team's titles
+class Title(models.Model):
+    title = models.CharField('title', max_length = 100)
+    
+    # Base called method returns the title
+    def __unicode__(self):
+        return self.title
+        
+# Model that holds the team's routes
+class Route(models.Model):
+    route = models.CharField('route', max_length = 10)
+    
+    # Base called method returns the route
+    def __unicode__(self):
+        return self.route
+        
+# Manager class that instantiates the creation of a new teammate
+class TeammateManager(BaseUserManager):
+    
+    # Base create teammate method
+    def _create_user(self, email, first_name, last_name, password,
+                     is_staff, is_superuser, **extra_fields):
+                    
+        # Define the time
+        now = timezone.now()
+        # Check for an email
+        if not email:
+            # If none, tell the user
+            raise ValueError('The teammate must set an email!')
+        # Clean and assign data
+        email = self.normalize_email(email)
+        user = self.model(email = email, is_staff = is_staff,
+                          is_active = True, is_superuser = is_superuser,
+                          last_login = now, first_name = first_name,
+                          last_name = last_name, date_joined = now,
+                          **extra_fields)
+        # Ready to insert into database
+        user.set_password(password)
+        user.save(using = self._db)
+        
+        return user
+    
+    # Method to create a normal teammate; has not access to admin panel
+    def create_user(self, email, first_name, last_name, password = None, **extra_fields):
+        # Utilizes the base creation method
+        return self._create_user(email, first_name, last_name, password, False, False, **extra_fields)
+        
+    # Method to create a 'superuser'; has access to EVERYTHING
+    def create_superuser(self, email, first_name, last_name, password, **extra_fields):
+        # Utilize the base creation method
+        return self._create_user(email, first_name, last_name, password, True, True, **extra_fields)
+
+# Model that holds the teammates' data
+class Teammate(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', max_length = 100, unique = True, db_index = True)
+    first_name = models.CharField('first name', max_length = 35)
+    last_name = models.CharField('last name', max_length = 35)
+    title = models.ForeignKey(Title, blank = True)
+    route = models.ForeignKey(Route, blank = True)
+    date_of_birth = models.DateField('date of birth', help_text = 'YYYY-MM-DD format')
+    is_staff = models.BooleanField('leadership status', default = False,
+                                   help_text = 'Designates who may login to the admin area')
+    is_active = models.BooleanField('active status', default = True,
+                                    help_text = 'Designates whether or not the teammate can login')
+    date_joined = models.DateTimeField('date added', default = timezone.now)
+    
+    # Defines the class that manages the teammate
+    objects = TeammateManager()
+    
+    # Defines what field will be used as the username
+    USERNAME_FIELD = 'email'
+    # Defines what field(s) will be required
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth']
+    
+    # Metadata about the model
+    class Meta:
+        verbose_name = 'teammate'
+        verbose_name_plural = 'teammates'
+        
+    # Methods to return values about the teammate
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        # Returns the full name
+        return full_name.strip()
+        
+    def get_short_name(self):
+        # Returns the first name
+        return self.first_name
+    
+    def get_email(self):
+        # Returns the email address
+        return self.email
+        
+    def get_title(self):
+        # Returns the title
+        return self.title
+        
+    def get_route(self):
+        # Returns the route
+        return self.route
+        
+    def get_dob(self):
+        # Returns the DOB
+        return self.date_of_birth
+        
+    # Base called function
+    def __unicode__(self):
+        return self.get_full_name()
