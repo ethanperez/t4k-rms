50df1d889106d5e26f6fb4c5538ac2f16b9f26c1
Ethan Perez <ethan@ethanperez.com>
2014-01-26 18:19:48 -0600

    Removed migrations from .gitignore

---
+++ fitness/migrations/0001_initial.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Ride'
+        db.create_table(u'fitness_ride', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['riders.Teammate'])),
+            ('date', self.gf('django.db.models.fields.DateField')()),
+            ('buddies', self.gf('django.db.models.fields.CharField')(max_length=300)),
+            ('miles', self.gf('django.db.models.fields.DecimalField')(max_digits=5, decimal_places=2)),
+            ('pace', self.gf('django.db.models.fields.DecimalField')(max_digits=3, decimal_places=1)),
+            ('duration', self.gf('durationfield.db.models.fields.duration.DurationField')()),
+            ('comments', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'fitness', ['Ride'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Ride'
+        db.delete_table(u'fitness_ride')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'fitness.ride': {
+            'Meta': {'object_name': 'Ride'},
+            'buddies': ('django.db.models.fields.CharField', [], {'max_length': '300'}),
+            'comments': ('django.db.models.fields.TextField', [], {}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'duration': ('durationfield.db.models.fields.duration.DurationField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'miles': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'}),
+            'pace': ('django.db.models.fields.DecimalField', [], {'max_digits': '3', 'decimal_places': '1'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['riders.Teammate']"})
+        },
+        u'riders.route': {
+            'Meta': {'object_name': 'Route'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'route': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        },
+        u'riders.teammate': {
+            'Meta': {'object_name': 'Teammate'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '35'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '35'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'route': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['riders.Route']", 'null': 'True'}),
+            'title': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['riders.Title']", 'null': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"})
+        },
+        u'riders.title': {
+            'Meta': {'object_name': 'Title'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['fitness']
---
+++ riders/migrations/0002_auto__chg_field_teammate_title__chg_field_teammate_route.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Teammate.title'
+        db.alter_column(u'riders_teammate', 'title_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['riders.Title'], null=True))
+
+        # Changing field 'Teammate.route'
+        db.alter_column(u'riders_teammate', 'route_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['riders.Route'], null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Teammate.title'
+        db.alter_column(u'riders_teammate', 'title_id', self.gf('django.db.models.fields.related.ForeignKey')(default='Blank', to=orm['riders.Title']))
+
+        # Changing field 'Teammate.route'
+        db.alter_column(u'riders_teammate', 'route_id', self.gf('django.db.models.fields.related.ForeignKey')(default='Blank', to=orm['riders.Route']))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'riders.route': {
+            'Meta': {'object_name': 'Route'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'route': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        },
+        u'riders.teammate': {
+            'Meta': {'object_name': 'Teammate'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '35'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '35'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'route': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['riders.Route']", 'null': 'True'}),
+            'title': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['riders.Title']", 'null': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"})
+        },
+        u'riders.title': {
+            'Meta': {'object_name': 'Title'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['riders']
