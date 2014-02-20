2a294bac9009f59b9591a7e6b17ef9aae6006f15
Ethan Perez <ethan@ethanperez.com>
2014-02-20 00:09:37 -0600

    Add logged time to ride records

--- fitness/models.py
+++ fitness/models.py
@@ -13,6 +13,7 @@ class Ride(models.Model):
     pace = models.DecimalField('average pace', max_digits = 3, decimal_places = 1)
     duration = DurationField('time to complete ride', help_text = 'HH:MM:SS format')
     comments = models.TextField('comments')
+    time_logged = models.DateTimeField('logged', auto_now_add = True)
 
     # Method to create a new ride
     def log(cls, user, date, buddies, miles, pace, duration, comments):
@@ -46,3 +47,7 @@ class Ride(models.Model):
     def get_duration(self):
         # Returns the time it took to ride
         return self.duration
+        
+    def get_logged(self):
+        # Return the time the ride was logged at
+        return self.time_logged
