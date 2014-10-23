ada0ddd4cb9c23323a1d181ec1777b62d8a261b5
Ethan Perez <ethan@ethanperez.com>
2014-10-23 14:52:06 -0500

    Add link model and settings

--- links/models.py
+++ links/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime, timedelta
+from durationfield.db.models.fields.duration import DurationField
+from riders.models import Teammate
+from django.utils import timezone
 
-# Create your models here.
+class Link(models.Model):
+	user = models.ForeignKey('riders.Teammate', verbose_name = "link")
+	kintera_id = models.IntegerField(blank = True)
+	url = models.CharField('short URL', max_length = 50)
+	clicks = models.IntegerField(default = 0)
+	last_click = models.DateTimeField('last clicked', auto_now_add = True)
+
+	class Meta:
+		verbose_name = 'link'
+		verbose_name_plural = 'links'
+
+	# Base unicode method
+	def __unicode__(self):
+		# Return the short url
+		return self.url
--- t4krms/settings.py
+++ t4krms/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = (
     'riders',
     'fitness',
     'dashboard',
+    'links',
 )
 
 MIDDLEWARE_CLASSES = (
